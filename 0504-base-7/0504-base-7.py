57c759354e567f2a43f45e9e640660dddf7f2842
Kushal D Soni <kushal.soni@somaiya.edu>
2026-08-24 19:45:21 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.72%) - LeetHub

--- 0504-base-7/0504-base-7.py
+++ 0504-base-7/0504-base-7.py
@@ -1,27 +1,17 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num<0:
-            num=-num
-            st=""
-            a=num%7
-            st+=str(a)
-            r=num//7
-            while r!=0:
-                a=r%7
-                st+=str(a)
-                r=r//7
-            st+="-"
-            return st[::-1]
-        else:
-            st=""
-            a=num%7
-            st+=str(a)
-            r=num//7
-            while r!=0:
-                a=r%7
-                st+=str(a)
-                r=r//7
-            return st[::-1]
-
-
+        if num == 0:
+            return "0"
+    
+        is_negative = num < 0
+        num = abs(num)
+        digits = []
         
+        while num > 0:
+            digits.append(str(num % 7))
+            num //= 7
+            
+        if is_negative:
+            digits.append("-")
+            
+        return "".join(reversed(digits))
