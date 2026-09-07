cbe194f7168633527e57e1bc6903a99c186b7a1c
Kushal D Soni <kushal.soni@somaiya.edu>
2026-09-07 11:14:38 +0530

    Time: 124 ms (12.41%), Space: 42 MB (5.17%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result =[]
+        def bt(f):
+            if f ==len(nums):
+                result.append(nums[:])
+                return result
+            for i in range(f,len(nums)):
+                nums[f],nums[i]=nums[i],nums[f]
+                bt(f+1)
+                nums[f],nums[i]=nums[i],nums[f]
+        bt(0)
+        unique = set(tuple(x) for x in result)
+        return [list(x) for x in unique]
+        
